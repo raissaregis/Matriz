a024763798411b5759c41d19413086f7d24da9c9
rah <raissa159@gmail.com>
2015-11-18 23:18:13 -0200

    ooi

---
+++ app/src/main/java/com/ifg/aluno/matriz/Operacoes.java
@@ -0,0 +1,32 @@
+package com.ifg.aluno.matriz;
+
+/**
+ * Created by aluno on 18/11/2015.
+ */
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+
+public class Operacoes extends Fragment{
+
+    public Operacoes() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.one_fragment, container, false);
+    }
+
+}
